6ccb5d1db4b2bf687938c9e2b9497b851ab82930
Code Head <codehead@codelegends.com>
2019-03-05 14:51:41 -0500

    setup webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+    .copy('node_modules/vuejs-datatable/dist/vuejs-datatable.js', 'public/js/vuejs-datatable/vuejs-datatable.js')
+    .copy('node_modules/moment/moment.js', 'public/js/moment/moment.js');
